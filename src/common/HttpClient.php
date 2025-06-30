lukas-w/docker-php-extensions

commit 1751f2f59eb697e71799e50de197c66ebb2a0df5

More HTTP request caching

src/common/HttpClient.php
@@ -14,7 +14,7 @@ class HttpClient
 	public array $headers = [];
 
 	public function __construct(
-		private readonly string $baseUrl,
+		private readonly string $baseUrl = '',
 	)
 	{
 		$ch = curl_init();
@@ -71,20 +71,10 @@ class HttpClient
 
 	public function get(string $path, bool $checkCode = true, mixed $out = null, array $headers = []): ?string
 	{
-		$cacheKey = "GET:$path";
-		$cacheValue = &$this->cache[$cacheKey];
-		if ($cacheValue ?? null) {
-			return $cacheValue;
-		}
+		[$code, $response] = $this->exec($path, $out, headers: $headers);
 
-		$response = $this->exec($path, $out, headers: $headers);
-		$code = curl_getinfo($this->ch, CURLINFO_HTTP_CODE);
-
-		if ($code === 200 || $code === 404) {
-			$cacheValue = $response;
-		}
 		if ($checkCode) {
-			$this->checkResponseCode('GET', $path);
+			$this->checkResponseCode('GET', $path, $code);
 		}
 
 		return is_string($response) ? $response : null;
@@ -92,12 +82,12 @@ class HttpClient
 
 	public function request(string $method, string $path, array $headers = []): string
 	{
-		return $this->exec($path, method: $method);
+		return $this->exec($path, method: $method)[1];
 	}
 
-	protected function checkResponseCode(string $method, string $path): void
+	protected function checkResponseCode(string $method, string $path, ?int $code = null): void
 	{
-		$code = curl_getinfo($this->ch, CURLINFO_HTTP_CODE);
+		$code ??= curl_getinfo($this->ch, CURLINFO_HTTP_CODE);
 		if ($code >= 200 && $code < 300) {
 			return;
 		}
@@ -107,8 +97,17 @@ class HttpClient
 		throw new RuntimeException('HTTP error: ' . $code, code: $code);
 	}
 
-	protected function exec(string $path, mixed $out = null, string $method = 'GET', array $headers = []): string|bool
+	protected function exec(string $path, mixed $out = null, string $method = 'GET', array $headers = []): array
 	{
+		if ($method === 'GET') {
+			$cacheKey = "$path:" . md5(json_encode($headers, JSON_THROW_ON_ERROR));
+
+			$cacheValue = &$this->cache[$cacheKey];
+			if ($cacheValue ?? null) {
+				return $cacheValue;
+			}
+		}
+
 		curl_setopt($this->ch, CURLOPT_CUSTOMREQUEST, $method);
 		curl_setopt($this->ch, CURLOPT_URL, $this->baseUrl . $path);
 		curl_setopt($this->ch, CURLOPT_HTTPHEADER, array_map(
@@ -126,11 +125,25 @@ class HttpClient
 			throw new InvalidArgumentException('Invalid output type: ' . gettype($out));
 		}
 
+		$responseHeaders = [];
+		curl_setopt($this->ch, CURLOPT_HEADERFUNCTION, static function ($_, string $data) use (&$responseHeaders): int {
+			$header = trim($data);
+			if (str_contains($header, ':')) {
+				[$name, $value] = explode(':', $header, 2);
+				$responseHeaders[strtolower($name)] = trim($value);
+			}
+			return strlen($data);
+		});
+
 		$response = curl_exec($this->ch);
 		if ($response === false) {
 			throw new RuntimeException('Curl error: ' . curl_error($this->ch));
 		}
 
-		return $response;
+		$code = curl_getinfo($this->ch, CURLINFO_HTTP_CODE);
+		if ($code >= 200 && $code < 500) {
+			$cacheValue = [$code, $response, $responseHeaders];
+		}
+		return [$code, $response, $responseHeaders];
 	}
 }

src/common/DockerRegistryHttpClient.php
@@ -8,6 +8,7 @@ use DateTimeImmutable;
 class DockerRegistryHttpClient extends HttpClient
 {
 	private array $tokens = [];
+	private HttpClient $tokenClient;
 
 	public function __construct(
 		string                   $baseUrl,
@@ -15,9 +16,10 @@ class DockerRegistryHttpClient extends HttpClient
 	)
 	{
 		parent::__construct($baseUrl);
+		$this->tokenClient = new HttpClient();
 	}
 
-	protected function exec(string $path, mixed $out = null, string $method = 'GET', array $headers = []): string|bool
+	protected function exec(string $path, mixed $out = null, string $method = 'GET', array $headers = []): array
 	{
 		$token = $this->getTokenFromCache($path);
 
@@ -25,22 +27,13 @@ class DockerRegistryHttpClient extends HttpClient
 			$this->headers["Authorization"] = "Bearer " . $token;
 		}
 
-		$wwwAuth = null;
-		curl_setopt($this->ch, CURLOPT_HEADERFUNCTION, function ($_, string $header) use (&$wwwAuth): int {
-			$wwwAuthH = 'www-authenticate:';
-			if (str_starts_with(strtolower($header), $wwwAuthH)) {
-				$wwwAuth = trim(substr($header, strlen($wwwAuthH)));
-			}
-			return strlen($header);
-		});
-
-		$response = parent::exec($path, $out, $method, $headers);
-		$code = curl_getinfo($this->ch, CURLINFO_HTTP_CODE);
+		[$code, $response, $responseHeaders] = parent::exec($path, $out, $method, $headers);
 
 		if ($code === 401) {
 			if ($token) {
 				throw new \RuntimeException("401 Unauthorized with token for $path");
 			}
+			$wwwAuth = $responseHeaders['www-authenticate'] ?? null;
 			if (!$wwwAuth) {
 				throw new \RuntimeException("401 but no WWW-Authenticate header returned");
 			}
@@ -49,7 +42,7 @@ class DockerRegistryHttpClient extends HttpClient
 			return $this->exec($path, $out);
 		}
 
-		return $response;
+		return [$code, $response];
 	}
 
 	/**
@@ -80,17 +73,17 @@ class DockerRegistryHttpClient extends HttpClient
 				throw new \RuntimeException("Missing required parameter '$required' in WWW-Authenticate header");
 			}
 		}
-		$client = new HttpClient($params['realm']);
-		unset($params['realm']);
 
 		if ($this->tokenAuth) {
-			$client->setHeaders([
+			$this->tokenClient->setHeaders([
 				"Authorization: Basic " . base64_encode($this->tokenAuth),
 			]);
 		}
 
+		$realm = $params['realm'];
+		unset($params['realm']);
 		try {
-			$r = $client->getJson('?' . http_build_query($params));
+			$r = $this->tokenClient->getJson($realm . '?' . http_build_query($params));
 		} catch (\Throwable $e) {
 			throw new \RuntimeException("Failed to request token for $path", previous: $e);
 		}

commit 47f20ee13bfac11634caf9c8d9384c9785af0f3a

Fix docker registry auth regression
Regression from 1751f2f59eb697e71799e50de197c66ebb2a0df5

src/common/HttpClient.php
@@ -99,6 +99,7 @@ class HttpClient
 
 	protected function exec(string $path, mixed $out = null, string $method = 'GET', array $headers = []): array
 	{
+		$headers = [...$this->headers, ...$headers];
 		if ($method === 'GET') {
 			$cacheKey = "$path:" . md5(json_encode($headers, JSON_THROW_ON_ERROR));
 
@@ -112,8 +113,8 @@ class HttpClient
 		curl_setopt($this->ch, CURLOPT_URL, $this->baseUrl . $path);
 		curl_setopt($this->ch, CURLOPT_HTTPHEADER, array_map(
 			static fn($k, $v) => "$k: $v",
-			array_keys($this->headers),
-			[...$this->headers, ...$headers],
+			array_keys($headers),
+			$headers,
 		));
 
 		if (is_resource($out)) {