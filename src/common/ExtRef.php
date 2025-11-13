lukas-w/docker-php-extensions

commit 9b34712d30dc0f508769b531380abdc5b370b1f3

Accept non-numeric versions

src/common/ExtRef.php
@@ -76,7 +76,7 @@ class ExtRef
 			'devel'
 		];
 		$channel_pattern = "(?<channel>" . implode('|', $channels) . ")";
-		$version_pattern = "(?<compatible>\^)?(?<version>[\d\.]+)";
+		$version_pattern = "(?<compatible>\^)?(?<version>[^\-@]+)";
 
 		$variants = [
 			"/^$version_pattern@$channel_pattern$/", // Version with channel, e.g. ^2.8@stable

tests/builder/VersionToolsTest.php
@@ -59,8 +59,14 @@ class VersionToolsTest extends TestCase
 	{
 		$this->assertEquals(
 			['1.2.3', '1.1.15'],
-			VersionTools::getLatestPatchVersions(
+			VersionTools::getLatestVersionsWithLevel(
 				['1.2.3', '1.2.2', '1.2.1', '1.1.15', '1.1.15-rc', '1.1.5']
+			)
+		);
+		$this->assertEquals(
+			['2.1.15', '1.2.3'],
+			VersionTools::getLatestVersionsWithLevel(
+				['1.2.3', '1.2.2', '1.2.1', '2.1.15', '2.1.15-rc', '2.1.5']
 			)
 		);
 	}

commit 874dab3043b1ce68cd3202ce712a1fe619866479

Fix parsing channel-only versions
Regression from 9b34712d30dc0f508769b531380abdc5b370b1f3

src/common/ExtRef.php
@@ -80,8 +80,8 @@ class ExtRef
 
 		$variants = [
 			"/^$version_pattern@$channel_pattern$/", // Version with channel, e.g. ^2.8@stable
-			"/^$version_pattern$/", // Version only, e.g. ^2.8, 2.8.1
 			"/^$channel_pattern$/", // Channel only, e.g. stable
+			"/^$version_pattern$/", // Version only, e.g. ^2.8, 2.8.1
 		];
 
 		foreach ($variants as $variant) {